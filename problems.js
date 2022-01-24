d5b848ace074b09a74e747bc242a19d7c5ac0891
breadfan18 <breadfan18@gmail.com>
2022-01-24 13:19:42 -0500

    getting back into this

--- problems.js
+++ problems.js
@@ -1,23 +1,19 @@
 /*
-### Challenge: 21-primeFactors
+### Challenge: 02-addTwoNumbers
 
-**Difficulty:** Intermediate
+**Difficulty:** Basic  
 
 Prompt:
 
-> Now that you have solved the last challenge of determining if a whole number is prime, let's expand upon that concept to...
-> Write a function named primeFactors that accepts a whole number greater than one (1) as an argument and returns an array of that argument's prime factors.
-> The prime factors of a whole number are the prime numbers that, when multiplied together, equals the whole number.
-> If the argument provided is not greater than 1, or not a whole number, then primeFactors should return an empty array.
+Write a function called addTwoNumbers that accepts two numeric arguments and returns the sum of those two numbers.
+If either argument is not a Number, return the value of NaN.
 
 Examples:
 
-primeFactors(2) --> [2]
-primeFactors(3) --> [3]
-primeFactors(4) --> [2, 2]
-primeFactors(18) --> [2, 3, 3]
-primeFactors(29) --> [29]
-primeFactors(105) --> [3, 5, 7]
-primeFactors(200) --> [2, 2, 2, 5, 5]
+addTwoNumbers(5, 10) --> 15
+addTwoNumbers(10, -2) --> 8
+addTwoNumbers(0, 0) --> 0
+addTwoNumbers('Hello', 5) --> NaN
 */
-// Your solution for 21-primeFactors here:
+// Your solution for 02-addTwoNumbers here:
+
