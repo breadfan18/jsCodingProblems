ed2bcfe77650c72d7d8d5fb1d7eacc5de61003ef
breadfan18 <breadfan18@gmail.com>
2022-02-05 15:28:09 -0500

    wiped out multiply

--- problems.js
+++ problems.js
@@ -1,29 +1,18 @@
-/*
-### Challenge: 05-computeRemainder
-
-**Difficulty:** Basic
-
-Prompt:
-
-> Write a function named computeRemainder that accepts two numeric arguments and returns the remainder of the division of those two numbers.
-> The first argument should be the dividend and the second argument should be the divisor.
-> If a 0 is passed in as the second argument you should return JavaScript's special numeric value: Infinity.
-> For extra fun, complete this ### Challenge without using the modulus (%) operator.
-
-Examples:
-
-computeRemainder(10,2) --> 0
-computeRemainder(4,0) --> Infinity
-computeRemainder(10.5, 3) --> 1.5
-*/
-// Your solution for 05-computeRemainder:
-function multiply(num1, num2) {
-    // return ('i').repeat(num1).repeat(num2).length
-    let total = num1;
-    for (let i = 1; i < num2; i++) {
-        total += num1 
-    }
-    return total;
-}
-
-console.log(multiply(3, 30));
+ /*
+  ### Challenge: 06-range
+  
+  **Difficulty:** basic
+  
+  Prompt:
+  
+  > Write a function called range that accepts two integers as arguments and returns an array of integers starting with the first argument up to one less than the second argument.
+  > The range function must be called with the first argument less than or equal to the second argument, otherwise return the string "First argument must be less than second".
+  
+  Examples:
+  
+  range(1,4) --> [1,2,3]
+  range(-2, 3) --> [-2,-1,0,1,2]
+  range(1,1) --> []
+  range(5,2) --> "First argument must be less than second"
+  */
+  // Your solution for 06-range here:
