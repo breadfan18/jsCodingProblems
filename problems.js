fc19e88872ea63b8d45ea4509019d3a106f9b2c2
breadfan18 <breadfan18@gmail.com>
2022-02-08 18:44:15 -0500

    still working on range

--- problems.js
+++ problems.js
@@ -6,7 +6,8 @@
   Prompt:
   
   > Write a function called range that accepts two integers as arguments and returns an array of integers starting with the first argument up to one less than the second argument.
-  > The range function must be called with the first argument less than or equal to the second argument, otherwise return the string "First argument must be less than second".
+  > The range function must be called with the first argument less than or equal to the second argument, 
+  otherwise return the string "First argument must be less than second".
   
   Examples:
   
@@ -16,3 +17,15 @@
   range(5,2) --> "First argument must be less than second"
   */
   // Your solution for 06-range here:
+
+function range(a, b){
+    if(a >= b) return 'First argument must be less than second'
+    const arr = [];
+    for (let i = a; i < b; i++) {
+        arr.push(i)
+    }
+    return arr;
+}
+
+console.log(range(1,10))
+// console.log('test');
