36095703023270f6f5e21721fec29c1e35a2903c
breadfan18 <breadfan18@gmail.com>
2022-02-09 22:20:26 -0500

    set for tomoro

--- problems.js
+++ problems.js
@@ -1,31 +1,43 @@
-  /*
-  ### Challenge: 15-mergeObjects
-  
-  **Difficulty:**  Intermediate
-  
-  Prompt:
-  
-  > Write a function named mergeObjects that accepts at least two objects as arguments, merges the properties of the second through
-  > n objects into the first object, then finally returns the first object.
-  > If any objects have the same property key, values from the object(s) later in the arguments list should overwrite earlier values.
-  
-  Examples:
-  
-  > mergeObjects({}, {a: 1});  --> {a: 1} (same object as first arg)
-  > mergeObjects({a: 1, b: 2, c: 3}, {d: 4});  --> {a: 1, b: 2, c: 3, d: 4}
-  > mergeObjects({a: 1, b: 2, c: 3}, {d: 4}, {b: 22, d: 44});  --> {a: 1, b: 22, c: 3, d: 44}
-  */
-  // Your solution for 15-mergeObjects here:
+/*
+### Challenge: 16-findHighestPriced
+ 
+**Difficulty:**  Intermediate
+ 
+Prompt:
+ 
+> Write a function named findHighestPriced that accepts a single array of objects.
+> The objects contained in the array are guaranteed to have a price property holding a numeric value.
+> The function should return the object in the array that has the largest value held in the price property.
+> If there's a tie between two or more objects, return the first of those objects in the array.
+> Return the original object, not a copy.
+> Do not mutate the array being passed in.
+ 
+Examples:
+ 
+findHighestPriced([
+  { sku: 'a1', price: 25 },
+  { sku: 'b2', price: 5 },
+  { sku: 'c3', price: 50 },
+  { sku: 'd4', price: 10 }
+]);
+--> { sku: 'c3', price: 50 }
+ 
+findHighestPriced([
+  { sku: 'a1', price: 25 },
+  { sku: 'b2', price: 50 },
+  { sku: 'c3', price: 50 },
+  { sku: 'd4', price: 10 }
+]);
+--> { sku: 'b2', price: 50 }
+*/
+// Your solution for 16-findHighestPriced here:
 
-  function mergeObjects(object1, ...objects) {
-    for (let i = 0; i < objects.length; i++) {
-        object1 = {...object1, ...objects[i]}
-    }
-    return object1
+function findHighestPriced() {
+    
 }
 
 
 
 
-console.log(mergeObjects({}, {a: 1}))
+console.log(findHighestPriced({}, { a: 1 }))
 // console.log('test');
