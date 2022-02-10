d81a9423012400d691881058f0daefc79bc16c5e
breadfan18 <breadfan18@gmail.com>
2022-02-09 22:16:05 -0500

    minor changes

--- problems.js
+++ problems.js
@@ -1,31 +1,31 @@
-/*
-### Challenge: 14-fromPairs
- 
-**Difficulty:** Intermediate
- 
-Prompt:
- 
-> Write a function named fromPairs that creates an object from an array containing nested arrays.
-> Each nested array will have two elements representing key/value pairs used to create key/value pairs in an object to be returned by the function.
-> If a key appears in multiple pairs, the rightmost pair should overwrite previous the previous entry in the object.
- 
-Examples:
- 
-fromPairs([ ['a', 1], ['b', 2], ['c', 3] ]) --> { a: 1, b: 2, c: 3 }
-fromPairs([ ['name', 'Sam"], ['age', 24], ['name', 'Sally'] ]) --> { name: "Sally", age: 24 }
-*/
-// Your solution for 14-fromPairs here:
+  /*
+  ### Challenge: 15-mergeObjects
+  
+  **Difficulty:**  Intermediate
+  
+  Prompt:
+  
+  > Write a function named mergeObjects that accepts at least two objects as arguments, merges the properties of the second through
+  > n objects into the first object, then finally returns the first object.
+  > If any objects have the same property key, values from the object(s) later in the arguments list should overwrite earlier values.
+  
+  Examples:
+  
+  > mergeObjects({}, {a: 1});  --> {a: 1} (same object as first arg)
+  > mergeObjects({a: 1, b: 2, c: 3}, {d: 4});  --> {a: 1, b: 2, c: 3, d: 4}
+  > mergeObjects({a: 1, b: 2, c: 3}, {d: 4}, {b: 22, d: 44});  --> {a: 1, b: 22, c: 3, d: 44}
+  */
+  // Your solution for 15-mergeObjects here:
 
-
-function fromPairs(arr) {
-    return arr.reduce((allValues, innerArr) => {
-        allValues[innerArr[0]] = innerArr[1]
-        return allValues
-    }, {})
+  function mergeObjects(object1, ...objects) {
+    for (let i = 0; i < objects.length; i++) {
+        object1 = {...object1, ...objects[i]}
+    }
+    return object1
 }
 
 
 
 
-console.log(fromPairs([['name', 'Sam'], ['age', 24], ['name', 'Sally']]))
+console.log(mergeObjects({}, {a: 1}))
 // console.log('test');
