a7544fe125054e158a67fc0dc1b488d5172581b8
breadfan18 <breadfan18@gmail.com>
2022-02-16 20:15:38 -0500

    solved getnumber for ip

--- README.md
+++ README.md
@@ -704,6 +704,48 @@ function isPrime(num) {
 
 
 
+### Challenge: 25-getNumForIP
+
+**Difficulty:** Intermediate
+
+Prompt:
+
+> Write a function called getNumForIP that accepts a single string as argument.
+> The input string is formatted as an IP address, such as '192.156.99.15'.  IP addresses are used in networking and are actually 32-bit integers.  However, those that work with networks find it more convenient to work with these numbers as four 8-bit integers, separated by a 'dot' character.
+> The getNumForIP function should return the numeric value of the string IP address being passed in as an argument.
+
+Hints:
+
+> Each 8-bit number can hold a value between 0 and 256.
+> An IP's right most 8-bit number represents how many of 256 raised to the power of 0 (equals 1) there are.  The next 8-bit number represents how many of 256 raised to the power of 1 (256) there are, etc.  For example, if you took the right-most two 8-bit numbers of the IP address 192.156.99.15, you would have 15 * (256 ** 0), which equals 15, and 99 * (256**1), which equals 25344.
+> To compute the numeric value for an IP address, you first compute the value for each of the four 8-bit chunks (as described in the above hint), and add them together!
+
+Examples:
+
+> getNumForIP( '0.0.0.1' ) // => 1
+> getNumForIP( '0.0.2.0' ) // => 512
+> getNumForIP( '192.156.99.15' ) // => 3231474447
+> getNumForIP( '10.0.0.1' ) // => 167772161
+
+```javascript
+/* 
+- First we split the string to get each number, then reverse the array to start from the last number first
+- Then, use reduce to update the value of output with the math, while also using the 3rd argument of reduce
+- the currentIndex in order to do the 'powerOf' calculation
+-- NOTE: ** calculates the power of (similar to Math.pow)
+*/
+
+function getNumForIP(str) {
+  const splitArr =  str.split('.').reverse();
+  const ipsum = splitArr.reduce((output, ip, idx) => {
+    output += parseInt(ip) * (256 ** idx);
+    return output
+  }, 0)
+
+  return ipsum;
+}
+```
+
 
 
 
--- problems.js
+++ problems.js
@@ -1,52 +1,52 @@
-  /*
-  ### Challenge: 22-intersection
-  
-  **Difficulty:** Intermediate
-  
-  Prompt:
-  
-  > Write a function named intersection that accepts two arguments which are both arrays. 
-  > The array arguments may contain any mixture of strings, numbers and/or booleans 
-  > but no reference types, i.e., objects.
-  > The function should return a new array containing all elements in common, 
-  > including repeating element values.
-  > The ordering of the elements in the returned is not important.
-  > If there are no elements in the arrays in common,  the intersection 
-  > function should return an empty array.
-  > The function should not mutate (change) either argument.
-  
-  Examples:
-  
-  intersection(['a', 1], []) --> []
-  intersection(['a', 1], [true, 'a', 15]) --> ['a']
-  intersection([1, 'a', true, 1, 1], [true, 1, 'b', 1]) --> [1, true, 1]
-  */
-  // Your solution for 22-intersection here:
+/*
+### Challenge: 25-getNumForIP
 
-  
-  
-  // Your solution for 20-isPrime here:
-function intersection(arr1, arr2) {
+**Difficulty:**  Intermediate
 
-  console.log(arr1.sort())
-  let output = []
-  let test = arr1.filter(element => {
-    return arr2.includes(element)
-  })
+Prompt:
 
-  return test
+> Write a function called getNumForIP that accepts a single string as argument.
+> The input string is formatted as an IP address, such as '192.156.99.15'.  
+> IP addresses are used in networking and are actually 32-bit integers.  
+> However, those that work with networks find it more convenient to work with these numbers 
+> as four 8-bit integers, separated by a 'dot' character.
+> The getNumForIP function should return the numeric value of the string IP address being 
+> passed in as an argument.
 
-  }
-  
+Hints:
+
+> Each 8-bit number can hold a value between 0 and 256.
+> An IP's right most 8-bit number represents how many of 256 raised to the power of 0 (equals 1) 
+> there are.  The next 8-bit number represents how many of 256 raised to the 
+> power of 1 (256) there are, etc.  For example, if you took the right-most two 8-bit numbers
+> of the IP address 192.156.99.15, you would have 15 * (256 ** 0), which equals 15, and 99 * (256**1), which equals 25344.
+> To compute the numeric value for an IP address, you first compute the value for 
+> each of the four 8-bit chunks (as described in the above hint), and add them together!
+
+Examples:
+
+getNumForIP( '0.0.0.1' ) // => 1
+getNumForIP( '0.0.2.0' ) // => 512
+getNumForIP( '192.156.99.15' ) // => 3231474447
+getNumForIP( '10.0.0.1' ) // => 167772161
+*/
+// Your solution for 25-getNumForIP here:
+function getNumForIP(str) {
+  const splitArr =  str.split('.').reverse();
+  const ipsum = splitArr.reduce((output, ip, idx) => {
+    output += parseInt(ip) * (256 ** idx);
+    return output
+  }, 0)
+
+  return ipsum;
+}
 
 
+console.log(getNumForIP('10.0.0.1'))
 
-
-console.log(intersection([1, 'a', true, 1, 1], [true, 1, 'b', 1]))
-
-/* 
-Given a string, we'll say that the front is the first 3 chars of the string. 
-If the string length is less than 3, the front is whatever is there. 
+/*
+Given a string, we'll say that the front is the first 3 chars of the string.
+If the string length is less than 3, the front is whatever is there.
 Return a new string which is 3 copies of the front.
 
 Examples
