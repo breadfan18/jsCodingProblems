e2b659611662d356fe8b4ec72537b93a8bcf883c
breadfan18 <breadfan18@gmail.com>
2022-02-16 20:21:23 -0500

    winning ticket in

--- problems.js
+++ problems.js
@@ -1,48 +1,37 @@
 /*
-### Challenge: 25-getNumForIP
+  ### Challenge: 24-isWinningTicket
+  
+  **Difficulty:**  Intermediate
+  
+  Prompt:
+  
+  > Write a function called isWinningTicket that accepts a single array an as argument.
+  > The input array represents a 'lottery ticket' consisting of one or more nested 2-value arrays.  
+  > The first value of a nested array will be a string, the second an integer.
+  > The isWinningTicket function should return true if all of the nested arrays have a character
+  > in the string whose numeric character code equals the integer (2nd value).
+  > If any of the nested arrays have a string where all of the character's character code 
+  > does not match the integer, then return false.
+  
+  Hints:
+  
+  > A character/string can be created from a character code using the String.fromCharCode() class method.
+  > A character within a string's character code can be obtained using the charCodeAt() string method.
+  
+  Examples:
+  
+  isWinningTicket( [ ['ABC', 65] ] ) // => true
+  isWinningTicket( [ ['ABC', 999], ['XY', 89] ] ) // => false
+  isWinningTicket( [ ['ABC', 66], ['dddd', 100], ['Hello', 108] ] ) // => true
+  isWinningTicket( [ ['ABC', 66], ['dddd', 15], ['Hello', 108] ] ) // => false
+  */
+  // Your solution for 24-isWinningTicket here:
+function isWinningTicket(arr) {
 
-**Difficulty:**  Intermediate
-
-Prompt:
-
-> Write a function called getNumForIP that accepts a single string as argument.
-> The input string is formatted as an IP address, such as '192.156.99.15'.  
-> IP addresses are used in networking and are actually 32-bit integers.  
-> However, those that work with networks find it more convenient to work with these numbers 
-> as four 8-bit integers, separated by a 'dot' character.
-> The getNumForIP function should return the numeric value of the string IP address being 
-> passed in as an argument.
-
-Hints:
-
-> Each 8-bit number can hold a value between 0 and 256.
-> An IP's right most 8-bit number represents how many of 256 raised to the power of 0 (equals 1) 
-> there are.  The next 8-bit number represents how many of 256 raised to the 
-> power of 1 (256) there are, etc.  For example, if you took the right-most two 8-bit numbers
-> of the IP address 192.156.99.15, you would have 15 * (256 ** 0), which equals 15, and 99 * (256**1), which equals 25344.
-> To compute the numeric value for an IP address, you first compute the value for 
-> each of the four 8-bit chunks (as described in the above hint), and add them together!
-
-Examples:
-
-getNumForIP( '0.0.0.1' ) // => 1
-getNumForIP( '0.0.2.0' ) // => 512
-getNumForIP( '192.156.99.15' ) // => 3231474447
-getNumForIP( '10.0.0.1' ) // => 167772161
-*/
-// Your solution for 25-getNumForIP here:
-function getNumForIP(str) {
-  const splitArr =  str.split('.').reverse();
-  const ipsum = splitArr.reduce((output, ip, idx) => {
-    output += parseInt(ip) * (256 ** idx);
-    return output
-  }, 0)
-
-  return ipsum;
 }
 
 
-console.log(getNumForIP('10.0.0.1'))
+console.log(isWinningTicket([ ['ABC', 999], ['XY', 89] ]))
 
 /*
 Given a string, we'll say that the front is the first 3 chars of the string.
